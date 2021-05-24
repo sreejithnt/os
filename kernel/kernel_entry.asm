f827e2236addc57d740c90e8d2cac132ef54373a
snt <sreejith@shopalyst.com>
2021-05-24 05:35:52 +0530

    moving kernel entry to call main to kernel rather than boot

---
+++ kernel/kernel_entry.asm
@@ -0,0 +1,7 @@
+[bits 32]		; by the time we reach here it will be in 32-bit protected mode
+[extern main]	; define the external symbol (main here) to look for
+
+call main		; make a call to main, thus landing in the correct 
+				; place in kernel
+
+jmp $
